9f7d25e3ff3d94d6637ccfa084a352b95270ba60
Tendy Hermansyah <55569231+TendyHemansyah@users.noreply.github.com>
2019-09-26 10:34:50 +0700

    Add files via upload

---
+++ src/main/java/com/mycompany/latihan6/Mamalia.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.latihan6;
+
+/**
+ *
+ * @author Tendy
+ * NAMA     : Tendy Hermansyah
+ * KELAS    : PBO2
+ * NIM      : 10118083
+ * Deskripsi Program : Kambing Static dan Konstanta
+ */
+public class Mamalia {
+//    variabel jumlahKambing dideklarasikan sebagai statik
+    public static int jumlahKambing; 
+}
